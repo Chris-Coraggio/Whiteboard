87878a36d49472a2684ee881d9e7b1ceb1db9905
Noah-Franks <nfranks@purdue.edu>
2018-04-21 16:16:19 -0400

    Node server startup

---
+++ node/server.js
@@ -0,0 +1,12 @@
+var http = require("http")
+
+// Exogenous control variables
+var server_port = 1776
+
+// Instantiate the server
+http.createServer(function (request, response) {
+
+}).listen(server_port);
+
+
+console.log('Http running on port 1776');
