e3fe0f1aa776e7dab7c0ae8a12bdde958fce34ce
Noah-Franks <nfranks@purdue.edu>
2018-04-21 16:49:58 -0400

    Commiting server state before reattempting with express tutorial

--- node/server.js
+++ node/server.js
@@ -8,12 +8,20 @@ var communications = express()
 // Exogenous control variables
 var server_port = 1776
 
-
 // Instantiate the server
 http.createServer(function (request, response) {
-
+    
 }).listen(server_port);
 
+communications.get('/', function(request, response) {
+    console.log(__dirname);
+    response.sendFile(__dirname + '/index.html');
+});
+
+communications.get('/index.html', function(request, response) {
+    response.sendFile(__dirname + '/index.html');
+});
 
 console.log('Http running on port 1776');
+
 
