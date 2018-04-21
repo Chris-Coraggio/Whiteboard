bde4fa03db10e60fc36e48432e3fc284727843e8
Noah-Franks <nfranks@purdue.edu>
2018-04-21 17:09:14 -0400

    Changed the root of the html tree to start at Zoe's directory. Alive.html can be requested to ensure files are being sent.

--- node/server.js
+++ node/server.js
@@ -8,16 +8,20 @@ var communications = express()
 
 // Exogenous control variables
 var server_port = 1776
+var html_tree = '/home/noah/Documents/Classes/cs252/lab6/Whiteboard/public'
+
+console.log('html root directory');
+console.log('\t' + html_tree);
 
 
 // Routes with side effects possible
 communications.get('/', function(request, response) {
-    console.log(__dirname);
-    response.sendFile(__dirname + '/index.html');
+    console.log(html_tree + '/index.html');
+    response.sendFile(html_tree + '/index.html');
 });
 
-communications.get('/index.html', function(request, response) {
-    response.sendFile(__dirname + '/index.html');
+communications.get('/alive.html', function(request, response) {
+    response.sendFile(html_tree + '/alive.html');
 });
 
 
