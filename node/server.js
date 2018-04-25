c8ca9b0911f714d2ff9930eb5b529ac517d44ebf
Noah-Franks <nfranks@purdue.edu>
2018-04-25 02:41:17 -0400

    Randomly generated cookies are generated for users, which will be furnished when a user logs in. Currently the login routine checks the password but doesn't do this yet

--- node/server.js
+++ node/server.js
@@ -19,16 +19,40 @@ console.log('MariaDB manager established\n');
 
 profiler.query('USE profiles;', function(err, rows) {});
 
-function user_exists(username, password, callback) {
+function user_exists(username, callback) {
     // Checks the database for an entry matching the username
     // Returns whether the user is in the database
     
     var check_string = 'SELECT 1 FROM humanity WHERE username = \'' + username + '\';';
     var result = profiler.query(check_string, function(err, rows) {
-        callback(username, password, rows.info.numRows > 0);
+        callback(rows.info.numRows > 0);
     });
 }
 
+function check_password(username, password, callback) {
+    // Checks the database for an entry with a matching username and password
+    // Returns whether the username and password match is found
+
+    var check_string = 'SELECT 1 FROM humanity WHERE username = \''
+        + username + '\' AND password = \'' + password + '\';'; 
+    
+    var result = profiler.query(check_string, function(err, rows) {
+        callback(rows.info.numRows > 0);
+    });
+}
+
+function make_cookie() {
+    // Generates and returns a 32 alphanumeric cookie
+    
+    var cookie = '';
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < 32; i++) {
+        cookie += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    }
+    
+    return cookie;
+}
 
 function register(username, password) {
     // Adds the user to the database
@@ -36,7 +60,7 @@ function register(username, password) {
 
     console.log('Registration request:');
     
-    user_exists(username, password, function(username, password, user_already_present) {
+    user_exists(username, function(user_already_present) {
 
         if (user_already_present) {
             console.log('\tUser already registered');
@@ -46,10 +70,11 @@ function register(username, password) {
         // User is definitely new, add them now to the database
         
         var registration_string;
-        registration_string  = 'INSERT INTO humanity VALUES (\'';
-        registration_string += username + '\', \'';
-        registration_string += password + '\', \'';
-        registration_string += username + '.txt\');';
+        registration_string  = 'INSERT INTO humanity VALUES (\''
+            + username +           '\', \''
+            + password +           '\', \''
+            + username +  '.txt' + '\', \''
+            + make_cookie()      + '\');';
 
         console.log('\t' + registration_string + '\t');
         
@@ -58,9 +83,41 @@ function register(username, password) {
         return true;
     });
 }
-                
 
-register('Zoe', 'wordpass');
+function login(username, password) {
+    // Logs the user in, providing a cookie
+    // First part in process, see callback routine
+
+    console.log('Login request:');
+    
+    user_exists(username, function(user_in_database) {
+
+        if (!user_in_database) {
+            console.log('\tUser not registered');
+            return;  // Could call error handling function instead
+        }
+        
+        // User is definitely new, check their login password before adding
+
+        check_password(username, password, function(password_correct) {
+
+            if (!password_correct) {
+                console.log('password was incorrect');
+                return false; // Could call error handling function instead
+            }
+            
+            // User has provided correct login credentials
+            
+        });
+    });
+}
+
+register('Zoe'  , 'wordpass');
+register('Noah' , 'password');
+register('Chris', 'pawordss');
+
+login('Zoe' , 'password');
+login('Noah', 'password');
 
 profiler.end();
 
